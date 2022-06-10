b2229f14a63b375ede2ba11f44046a846d7bdc47
meii11 <42169302+meii11@users.noreply.github.com>
2022-06-10 10:54:46 +0800

    对人物剪裁进行了测试0610

---
+++ dig_person_experiment/0610_dig_crop/crop_digPerson.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+Created by MeiiCho on 2022/06/10
+"""
+import os
+
+import cv2
+
+
+def main():
+    video = cv2.VideoCapture('../data/4_2_677_dig.mp4')
+    ret, frame = video.read()
+    height, width = frame.shape[:2]
+
+    half_w = width // 2
+    frame_normal, frame_mask = frame[:, :half_w, :], frame[:, half_w:, :]
+    crop = {'top': 55, 'left': 39, 'width': 113, 'circle': 'true', 'height': 113}
+    cv2.imwrite('../data/formal.png', frame_normal)
+    os.system(f"ffmpeg -i ../data/formal.png -vf 'drawbox=x=39:y=55:w=113:h=113:color=red@0.5' ../data/drawbox.png")
+    # crop_frame = frame_normal[crop['top']:crop['top'] + crop['height'],
+    #              crop['left']:crop['left'] + crop['width']]
+    # cv2.imwrite('../data/after_crop.png', crop_frame)
+
+
+if __name__ == '__main__':
+    main()
+    # {'id': 'huayuan8_mouth', 'top': 2, 'crop': {'top': 55, 'left': 39, 'width': 113, 'circle': 'true', 'height': 113},
+    #  'left': 677, 'text': {'content': '测试文本框的输入效果，所以文案不重要'}
