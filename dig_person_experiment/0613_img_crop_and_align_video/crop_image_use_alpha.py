396e3a9b8157e0134eb7c1fc3437f246aa8bf6df
meii11 <42169302+meii11@users.noreply.github.com>
2022-06-13 21:49:17 +0800

    优化图片剪裁逻辑，对正坐标也用同样的方法处理 0613

--- dig_person_experiment/0613_img_crop_and_align_video/crop_image_use_alpha.py
+++ dig_person_experiment/0613_img_crop_and_align_video/crop_image_use_alpha.py
@@ -10,13 +10,20 @@ import numpy as np
 
 
 def main():
-    img = '../data/dog.png'
+    """
+    {"top": 420, "url": "https://public-1255423687.cos.ap-shanghai.myqcloud.com/image-ele1655117797854",
+         "crop": {"top": -21, "left": 167, "width": 80, "height": 96}, "left": 87, "text": {}, "width": 250, "height": 300, "duration": 5, "layer_order": 6},
+    {"top": 622, "url": "https://public-1255423687.cos.ap-shanghai.myqcloud.com/image-ele1655117807149", "crop": {"top": 272, "left": 550, "width": 374, "height": 449},
+         "left": 467, "text": {}, "width": 250, "height": 300, "duration": 5, "layer_order": 1}
+    """
+    img = '../data/budala.png'
     w_bkg = '../data/white_bkg_1080_1920.jpg'
     # alpha_img = np.full(())
 
     # 925 * 520
-    crop_size = {'top': -50, 'left': -100, 'width': 500, 'height': 300}
-
+    crop_size = {'top': -21, 'left': 167, 'width': 80, 'height': 96}
+    img_width = 250
+    img_height = 300
     # c_height = crop_size['top'] + crop_size['height'] if crop_size['top'] > 0 else -crop_size['top'] + crop_size[
     #     'height']
     # c_width = crop_size['left'] + crop_size['width'] if crop_size['left'] > 0 else -crop_size['left'] + crop_size[
@@ -24,8 +31,10 @@ def main():
     alpha_img = np.full((crop_size['height'], crop_size['width'], 4), 255)
     cv2.imwrite('../data/alpha.png', alpha_img)
     alpha_img = '../data/alpha.png'
-    new_c_height = min(crop_size['height'] if crop_size['top'] > 0 else crop_size['top'] + crop_size['height'], 520)
-    new_c_width = min(crop_size['width'] if crop_size['left'] > 0 else crop_size['left'] + crop_size['width'], 925)
+    new_c_height = min(crop_size['height'] if crop_size['top'] > 0 else crop_size['top'] + crop_size['height'],
+                       img_height)
+    new_c_width = min(crop_size['width'] if crop_size['left'] > 0 else crop_size['left'] + crop_size['width'],
+                      img_width)
     new_top = -crop_size['top'] if crop_size['top'] < 0 else 0
     new_left = -crop_size['left'] if crop_size['left'] < 0 else 0
 
@@ -34,12 +43,12 @@ def main():
     os.system(f"ffmpeg -y -i {img} -vf crop=w={new_c_width}:h={new_c_height}:x={new_left}:y={new_top} {resize_img}")
 
     # overlay
-    os.system(f"ffmpeg -y -i {alpha_img} -i {resize_img} -filter_complex 'overlay={new_left}:{new_top}' -loglevel error ../data/crop_dog_f.png")
+    os.system(
+        f"ffmpeg -y -i {alpha_img} -i {resize_img} -filter_complex 'overlay={new_left}:{new_top}' -loglevel error ../data/crop_dog_f.png")
 
     # overlay on bkg
-    os.system(f"ffmpeg -y -i {w_bkg} -i ../data/crop_dog_f.png -filter_complex 'overlay={new_left}:{new_top}' -loglevel error ../data/crop_dog_f.png")
-
-
+    os.system(
+        f"ffmpeg -y -i {w_bkg} -i ../data/crop_dog_f.png -filter_complex 'overlay={new_left}:{new_top}' -loglevel error ../data/crop_dog_ff.png")
 
     # code = os.system(
     #     f"ffmpeg -y -threads {t_num} -i {dir_m1} -i {dir_m2} "
@@ -47,10 +56,37 @@ def main():
     pass
 
 
+def positive_coo():
+    img = '../data/new_dog.png'
+    w_bkg = '../data/white_bkg_1080_1920.jpg'
+    # {"top": 622, "url": "https://public-1255423687.cos.ap-shanghai.myqcloud.com/image-ele1655117807149",
+    #  "crop": {"top": 272, "left": 550, "width": 374, "height": 449},
+    #  "left": 467, "text": {}, "width": 250, "height": 300, "duration": 5, "layer_order": 1}
+    crop_size = {"top": 272, "left": 550, "width": 374, "height": 449}
+    height = 300
+    width = 250
+
+    alpha_img = np.full((crop_size['height'], crop_size['width'], 4), 255)
+    cv2.imwrite('../data/alpha.png', alpha_img)
+
+    c_img = cv2.imread(img)
+    image = c_img[crop_size['top']:crop_size['top'] + crop_size['height'],
+            crop_size['left']:crop_size['left'] + crop_size['width']]
+
+    resize_img = '../data/resize_dog.png'
+    cv2.imwrite(resize_img, image)
+    os.system(
+        f"ffmpeg -y -i {tmp_img} -vf scale={resize[0]}:{resize[1]} -loglevel error {resize_img}")
+    os.system(
+        f"ffmpeg -y -i {w_bkg} -i {resize_img} -filter_complex 'overlay=0:0' -loglevel error ../data/crop_dog_ff.png")
+
+
 if __name__ == '__main__':
     # os.system('rm -f 1 2')
-    main()
-    # d = {'fps': 30, 'stories': [
+    # main()
+    positive_coo()
+    # d = {'
+    # fps': 30, 'stories': [
     #     {'fps': 30,
     #      'order': 1,
     #      'lay_out': {
@@ -70,16 +106,19 @@ if __name__ == '__main__':
     #      'video_type': 'mp4', 'time_now': '10_0_15',
     #      'base': '/data/caopei/1-code/BackgroungCombination_chopei/local/985848103393296384'}
 
-    d = {'fps': 25, 'stories': [{'fps': 25, 'order': 1, 'lay_out': {'text': [
-        {'top': 72, 'font': '黑体', 'left': 260, 'color': '#000000', 'width': 525, 'height': 100, 'content': 'Sb', 'duration': 5, 'font_size': 14, 'layer_order': 2, 'is_animation': 'false'}
-    ], 'charts': [], 'images': [
-        {'top': 0, 'url': 'https://public-1255423687.cos.ap-shanghai.myqcloud.com/bg_import_2_1654606783882.jpg',
-         'crop': {}, 'left': 0, 'text': {}, 'width': 1080, 'height': 1920, 'duration': 30, 'layer_order': 0},
-        {'top': 362, 'url': 'https://public-1255423687.cos.ap-shanghai.myqcloud.com/image-ele1655103439527',
-         'crop': {'top': 122, 'left': 863, 'width': 1514, 'height': 2187},
-         'left': 237, 'text': {}, 'width': 555, 'height': 802, 'duration': 5, 'layer_order': 1}
-    ], 'videos': [], 'persons': [
-        {'id': '009sit', 'top': 1245,
-         'crop': {'top': 0, 'left': 0, 'width': 768, 'circle': 'false', 'height': 1080},
-         'left': 600, 'text': {'content': '你大爷的，烦人精'}, 'audio': 'yina', 'speed': 1, 'width': 475, 'height': 670, 'duration': 5, 'layer_order': 4}
-    ]}, 'batch_no': '985923062270525440'}], 'batch_no': '985923062270525440', 'resolution': '1080*1920', 'video_type': 'mp4', 'time_now': '15_17_25', 'base': '/data/caopei/1-code/BackgroungCombination_chopei/local/985923062270525440'}
+    d = {"fps": 30, "stories": [{"fps": 30, "order": 1, "lay_out": {"text": [], "charts": [], "images": [
+        {"top": 0, "url": "https://public-1255423687.cos.ap-shanghai.myqcloud.com/bg_import_1652339586034.jpg",
+         "crop": {}, "left": 0, "text": {}, "width": 1080, "height": 1920, "duration": 30, "layer_order": 0},
+        {"top": 420, "url": "https://public-1255423687.cos.ap-shanghai.myqcloud.com/image-ele1655117797854",
+         "crop": {"top": -21, "left": 167, "width": 80, "height": 96}, "left": 87, "text": {}, "width": 250,
+         "height": 300, "duration": 5, "layer_order": 6},
+        {"top": 622, "url": "https://public-1255423687.cos.ap-shanghai.myqcloud.com/image-ele1655117807149",
+         "crop": {"top": 272, "left": 550, "width": 374, "height": 449},
+         "left": 467, "text": {}, "width": 250, "height": 300, "duration": 5, "layer_order": 1}],
+
+                                                                    "videos": [], "persons": [
+            {"id": "007red", "top": 50,
+             "crop": {"top": 114, "left": 111, "width": 577, "circle": "false", "height": 811}, "left": 787,
+             "text": {"content": "测试负值，回显"}, "audio": "s019", "speed": 1, "width": 240, "height": 337, "duration": 5,
+             "layer_order": 4}]}, "batch_no": "985983935953502208"}], "batch_no": "985983935953502208",
+         "resolution": "1080*1920", "video_type": "mp4"}
