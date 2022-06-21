c6d1d62374c363d948ed535578f51de22da02a3a
meii11 <42169302+meii11@users.noreply.github.com>
2022-06-21 10:36:40 +0800

    圆形头像背景替换

--- dig_person_experiment/0621_concat_alpha_img/concat_alpha_png.py
+++ dig_person_experiment/0621_concat_alpha_img/concat_alpha_png.py
@@ -6,7 +6,7 @@ Created by MeiiCho on 2022/06/21
 
 
 def main():
-    pass
+    os.system("ffmpeg -y -i ./qiyu_500.png -vf crop=3000:3000:0:0 ./circle_data/bkg_square.png")
 
 
 if __name__ == '__main__':
